210915726e2ef173f474aa6b6748a701df7de852
Unfectum <roma509000@gmail.com>
2021-10-31 23:01:03 +0200

    axios retry

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
+
+axiosRetry(axios, { retries: 10 });
 
 export default axios.create({
-  baseURL: 'https://eerie-goblin-01597.herokuapp.com'
+  baseURL: 'https://eerie-goblin-01597.herokuapp.com',
+  timeout: 8000
 });
